46c55e326de0f5961d925e03ba63bc77fe7d2667
trie168 <trie168@gmail.com>
2024-05-12 15:12:36 +0700

    first commit

---
+++ routes/web.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
+
+Route::get('/', function () {
+    return view('pages.auth.auth-login');
+});
+
+Route::middleware(['auth'])->group(function () {
+    Route::get('home', function () {
+        return view('pages.dashboard', ['type_menu' => 'home']);
+    })->name('home');
+
+    Route::resource('users', UserController::class);
+});
